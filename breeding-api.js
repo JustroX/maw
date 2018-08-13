8115cc8b2d3afb1748ec126e90066d93279dba82
Justine Che T. Romero <thepiesaresquared@gmail.com>
2018-08-13 13:31:27 +0800

    Initial Commit

---
+++ breeding-api.js
@@ -0,0 +1,26 @@
+/********************
+
+Maw Breeding API
+(c) 2018 Eternity Echo
+
+Authors:
+Justine Che T. Romero
+
+*******************/
+
+var app; //express app
+
+
+/*
+	database overview
+	1. Geneset
+	2. Gene
+	3. Phenotype
+*/
+
+
+exports.init = (app)=>
+{
+	app.post('/breeding/api/assets/add',(req,res)=>{}); //add 
+	app.post('/breeding')
+}
